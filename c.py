7dfbf94b87b96ee858de60063df8c29447c26d67
LB-0204NWB <longbach0204@gmail.com>
2024-05-13 19:57:25 +0700

    13-5-4

--- c.py
+++ c.py
@@ -1,39 +1,46 @@
 import cv2
-import time
+import mediapipe as mp
 
-# Mở camera
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=2,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7)
+
+# Tùy chỉnh cho các điểm quan trọng và đường nối
+point_color = (44, 10, 15)  # Màu xanh lá
+connection_color = (0, 250, 100)  # Màu đỏ
+point_diameter = 2
+line_thickness = 1
+
+point_spec = mp.solutions.drawing_utils.DrawingSpec(color=point_color, thickness=point_diameter, circle_radius=point_diameter)
+connection_spec = mp.solutions.drawing_utils.DrawingSpec(color=connection_color, thickness=line_thickness)
+
 cap = cv2.VideoCapture(0)
 
-# Kiểm tra camera có mở thành công không
-if not cap.isOpened():
-    print("Không thể mở camera")
-    exit()
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        continue
 
-prev_time = 0  # Lưu thời gian của khung hình trước đó
+    # Chuyển đổi màu từ BGR sang RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Không thể nhận được khung hình từ camera. Thoát...")
+    # Vẽ các điểm quan trọng của tay lên hình ảnh
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp.solutions.drawing_utils.draw_landmarks(
+                image, hand_landmarks, mp_hands.HAND_CONNECTIONS,
+                landmark_drawing_spec=point_spec,
+                connection_drawing_spec=connection_spec)
+
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == ord('q'):  # Nhấn ESC để thoát
         break
 
-    # Tính FPS
-    current_time = time.time()
-    fps = 1 / (current_time - prev_time) if prev_time != 0 else 0
-    prev_time = current_time
-    
-    # Chuyển đổi giá trị fps thành chuỗi để hiển thị
-    fps_text = f"FPS: {int(fps)}"
-
-    # Hiển thị FPS trên khung hình
-    cv2.putText(frame, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-    # Hiển thị khung hình
-    cv2.imshow('Camera', frame)
-    print(current_time)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Giải phóng tài nguyên và đóng cửa sổ
+hands.close()
 cap.release()
 cv2.destroyAllWindows()
--- d.py
+++ d.py
@@ -1,16 +1,28 @@
 import cv2
 import time
+import mediapipe as mp
 
-cap =  cv2.VideoCapture(0)
+
+cap = cv2.VideoCapture(0)
+
+prev_time = 0
 
 while True:
-    ret, fame = cap.read()
-    
-    fame_rezi = cv2.resize(fame,(320,240))
-    cv2.imshow('camera', fame_rezi)
-    if cv2.waitKey(1) & 0xFF == ord('q'):  # Đợi 1 ms, và kiểm tra xem có nhấn phím 'q' để thoát không
-        break
+    bin, frame = cap.read()
 
+    frame_resize = cv2.resize(frame,(320,240))
+    
+    current_time = time.time()
+    fps = 1/ (current_time - prev_time)
+    prev_time = current_time
+    
+    fps_text = f"FPS {int(fps)}"
+
+    cv2.putText(frame_resize,fps_text,(30,30),cv2.FONT_HERSHEY_PLAIN,0.5,(100,100,0),2)
+
+    cv2.imshow('hand',frame_resize)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
